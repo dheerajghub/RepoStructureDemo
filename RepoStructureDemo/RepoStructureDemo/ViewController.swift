a307b17a389762a529fc320b0d82ddc3cc4e18e4
dheerajiosdev <dheeraj@appscrip.co>
2023-03-16 18:20:44 +0530

    initial commit

---
+++ RepoStructureDemo/RepoStructureDemo/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  RepoStructureDemo
+//
+//  Created by Dheeraj Kumar Sharma on 16/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - PROPERTIES
+    
+    
+    // MARK: - MAIN
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setupConstraints()
+    }
+    
+    // MARK: - FUNCTIONS
+    
+    func setupViews(){
+        view.backgroundColor = .red
+    }
+    
+    func setupConstraints(){
+        
+    }
+
+}
+
